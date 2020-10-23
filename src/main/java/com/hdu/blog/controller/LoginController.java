db5cf3c7d33aa4cc0c2b41a74375cc7c664396ac
HuChenhui <1005547224@qq.com>
2020-10-23 18:54:44 +0800

    1.因本地前端网页端口为8080，服务端口改为8080 2.ResultVO的属性由result改为data，配合前端 3.新增LoginController测试登陆，初步对接前端

---
+++ src/main/java/com/hdu/blog/controller/LoginController.java
@@ -0,0 +1,27 @@
+package com.hdu.blog.controller;
+
+import com.hdu.blog.core.domain.ResultVO;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class LoginController {
+    /*account  password*/
+    @PostMapping("/login")
+    @ApiOperation(value = "登录接口！")
+    @CrossOrigin
+    public ResultVO userLogin(@RequestBody Map<String,String> map){
+        Map<String,String> token = new HashMap<>();
+        token.put("Oauth-Token","9c5b89bd-ecb0-4c2c-8aae-9653d0e15e04");
+        return ResultVO.ok("成功",token);
+    }
+    @GetMapping("/users/currentUser")
+    @ApiOperation(value = "登录接口！")
+    public ResultVO currentUser(@RequestBody Map<String,String> map){
+
+        return null;
+    }
+}
