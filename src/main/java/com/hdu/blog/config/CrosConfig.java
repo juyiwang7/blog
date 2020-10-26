f816243f491aa56ce276fd58e2e831d11c81dd83
cellphone7 <shinianni520@qq.com>
2020-10-26 21:02:58 +0800

    ..

--- src/main/java/com/hdu/blog/config/CrosConfig.java
+++ src/main/java/com/hdu/blog/config/CrosConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CrosConfig implements WebMvcConfigurer {
 
+
     //重写方法
     @Override
     public void addCorsMappings(CorsRegistry registry) {
